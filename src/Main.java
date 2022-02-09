2069d2fe0bb6b5d0a556832e25c822a5ae873e42
boxer23541 <alexmb2354@gmail.com>
2022-02-09 18:00:42 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter your age");
+        int age = 18;
+
+        if (age >= 21)
+        {
+            System.out.println("You get a wristband");
+        }
+
+    }
+}
